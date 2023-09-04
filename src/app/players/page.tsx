ea6190695f5c8fc7ca810bcac1e8dd7822f142be
Andres <alizoandres11@gmail.com>
2023-09-04 15:46:57 -0400

    Bugfix inputs

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -118,36 +118,12 @@ export default function Players() {
                                 <CardContent className="grid gap-6">
                                     <div className="grid grid-cols-2 gap-4">
                                     <div className="grid gap-2">
-                                        <Label htmlFor="area">Who</Label>
-                                        <Select defaultValue="billing">
-                                        <SelectTrigger id="area">
-                                            <SelectValue placeholder="Select" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectItem value="team">Team</SelectItem>
-                                            <SelectItem value="billing">Billing</SelectItem>
-                                            <SelectItem value="account">Account</SelectItem>
-                                            <SelectItem value="deployments">Deployments</SelectItem>
-                                            <SelectItem value="support">Support</SelectItem>
-                                        </SelectContent>
-                                        </Select>
+                                        <Label htmlFor="description">Who</Label>
+                                        <Input></Input>
                                     </div>
                                     <div className="grid gap-2">
                                         <Label htmlFor="security-level">Against To</Label>
-                                        <Select defaultValue="2">
-                                        <SelectTrigger
-                                            id="security-level"
-                                            className="line-clamp-1 w-[160px] truncate"
-                                        >
-                                            <SelectValue placeholder="Select level" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            <SelectItem value="1">Severity 1 (Highest)</SelectItem>
-                                            <SelectItem value="2">Severity 2</SelectItem>
-                                            <SelectItem value="3">Severity 3</SelectItem>
-                                            <SelectItem value="4">Severity 4 (Lowest)</SelectItem>
-                                        </SelectContent>
-                                        </Select>
+                                        <Input></Input>
                                     </div>
                                     </div>
                                     <div className="grid gap-2">
